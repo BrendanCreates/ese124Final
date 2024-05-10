2630f6a95aee37aa0ce72928401a5e0af7a5f8c4
BrendanNevils <brendan.nevils@stonybrook.edu>
2024-05-09 20:28:44 -0400

    Need last 4 functions done and error in stack.c

--- stack.c
+++ stack.c
@@ -3,97 +3,83 @@
 #include <string.h>
 #include "stack.h"
 
-#define MAXSIZEOFMAZE 100
-
-
-void mark(Coordinates coordinates) { //mark position with pheromone
-
+void mark(char (*arr)[MAX_SIZE], Coordinates *coordinates) { //mark position with pheromone
+    Coords current = coordinates->coords[coordinates->top];
+    arr[current.x][current.y] = '+'; // Mark current position with '+'
 }
 
-void move_f(Coordinates coordinates) { //move forward one
-    coordinates.coords[coordinates.top].x++; //increment x
-    push(coordinates.coords[coordinates.top], coordinates);  //push updated position onto stack
+void move_f(Coordinates *coordinates) { //move forward one
+    coordinates->coords[coordinates->top].x++; //increment x
 }
 
-void move_b(Coordinates coordinates) { //move backward one
-    coordinates.coords[coordinates.top].x--; //decrement x
-    push(coordinates.coords[coordinates.top], coordinates);  //push updated position onto stack
+void move_b(Coordinates *coordinates) { //move backward one
+    coordinates->coords[coordinates->top].x--; //decrement x
 }
 
-void move_l(Coordinates coordinates) { //move left one
-    coordinates.coords[coordinates.top].y--; //decrement y
-    push(coordinates.coords[coordinates.top], coordinates); //push updated position onto stack
+void move_l(Coordinates *coordinates) { //move left one
+    coordinates->coords[coordinates->top].y--; //decrement y
 }
 
-void move_r(Coordinates coordinates) { //move right one
-    coordinates.coords[coordinates.top].y++; //increment y
-    push(coordinates.coords[coordinates.top], coordinates); //push updated position onto stack    
+void move_r(Coordinates *coordinates) { //move right one
+    coordinates->coords[coordinates->top].y++; //increment y
 }
 
-int cwf(char arr[], Coordinates coordinates) { //checks if the next locations in the front are pheromone free
-    Coords current = peek(coordinates);
-    int i;
-    for (i = 1; current.x + i < MAXSIZEOFMAZE; i++) {
+int cwf(char arr[][MAX_SIZE], Coordinates *coordinates) {
+    Coords current = coordinates->coords[coordinates->top]; // Access the last element in the array
+    for (int i = 1; current.x + i < MAX_SIZE; i++) { // Corrected loop condition
         if (arr[current.x + i][current.y] == '*')
             return 0;
     }
     return 1;
 }
 
-int cwb(char arr[], Coordinates coordinates) { //checks if the next locations in the back are pheromone free
+int cwb(char arr[][MAX_SIZE], Coordinates *coordinates) { //checks if the next locations in the back are pheromone free
+    Coords current = coordinates->coords[coordinates->top];
     int i;
-    while (i = 0; arr[coordinates.coords[coordinates.top].x - i] != '*' || arr[coordinates.coords[coordinates.top].x - i] != '+' && coordinates.coords[coordinates.top].x - i != MAXSIZEOFMAZE; i++) {
-
+    for (i = 1; current.x - i <= 0; i++) {
+        if (arr[current.x - i][current.y] == '*')
+            return 0;
     }
-    return
-
-    //Coords current = peek(coordinates);
-    //int i;
-    //for (i = 1; current.x - i < MAXSIZEOFMAZE; i++) {
-        //if (arr[current.x - i][current.y] == '*')
-            //return 0;
-    //}
-    //return 1;
-//}
-
+    return 1;
 }
 
-int cwl(char arr[], Coordinates coordinates) { //checks if the next locations to the left are pheromone free
-    Coords current = peek(coordinates);
+int cwl(char arr[][MAX_SIZE], Coordinates *coordinates) {
+    Coords current = coordinates->coords[coordinates->top]; // Access the last element in the array
     int i;
-    for (i = 1; current.y - i >= 0; i++) {
+    for (i = 1; current.y - i >= 0; i++) { // Corrected loop condition
         if (arr[current.x][current.y - i] == '*')
             return 0;
     }
     return 1;
 }
 
-int cwr(char arr[], Coordinates coordinates) { //checks if the next locations to the right are pheromone free
-    Coords current = peek(coordinates);
+
+int cwr(char arr[][MAX_SIZE], Coordinates *coordinates) { //checks if the next locations to the right are pheromone free
+    Coords current = coordinates->coords[coordinates->top];
     int i;
-    for (i = 1; current.y + i < MAXSIZEOFMAZE; i++) {
+    for (i = 1; current.y + i < 10; i++) {
         if (arr[current.x][current.y + i] == '*')
             return 0;
     }
     return 1;
 }
 
-void push(Coords coord, Coordinates coordinates) {
-    coordinates.top += 1;
-    coordinates.coords[coordinates.top] = coord;
+void push(Coords coord, Coordinates *coordinates) {
+    coordinates->top += 1;
+    coordinates->coords[coordinates->top] = coord;
 }
 
-Coords pop(Coordinates coordinates) {
-    Coords temp = coordinates.coords[coordinates.top];
-    coordinates.top = coordinates.top - 1;
+Coords pop(Coordinates *coordinates) {
+    Coords temp = coordinates->coords[coordinates->top];
+    coordinates->top = coordinates->top - 1;
     return temp;
 }
 
-Coords peek(Coordinates coordinates) {
-    return coordinates.coords[coordinates.top];
+Coords peek(Coordinates *coordinates) {
+    return coordinates->coords[coordinates->top];
 }
 
-void clear(Coordinates coordinates) {
-    coordinates.top = -1;
+void clear(Coordinates *coordinates) {
+    coordinates->top = -1;
 }
 
--- stack.h
+++ stack.h
@@ -1,6 +1,7 @@
 #ifndef STACK_H
 #define STACK_H
 #define MAX_SIZE 100
+// max size is index not length
 
 typedef struct coord {
     int x;
@@ -13,18 +14,22 @@ typedef struct coordinates {
     int top;
 } Coordinates;
 
-void mark(Coordinates coordinates);
-void move_f(Coordinates coordinates);
-void move_b(Coordinates coordinates);
-void move_l(Coordinates coordinates);
-void move_r(Coordinates coordinates);
-int cwf(char arr[], Coordinates coordinates);
-int cwb(char arr[], Coordinates coordiantes);
-int cwl(char arr[], Coordinates coordinates);
-int cwr(char arr[], Coordinates coordinates);
-void push(Coords coord, Coordinates coordinates);
-Coords pop(Coordinates coordinates);
-Coords peek(Coordinates coordinates);
-void clear(Coordinates coordinates);
+void mark(char (*arr)[MAX_SIZE], Coordinates *coordinates);
+void move_f(Coordinates *coordinates);
+void move_b(Coordinates *coordinates);
+void move_l(Coordinates *coordinates);
+void move_r(Coordinates *coordinates);
+int cwf(char arr[][MAX_SIZE], Coordinates *coordinates);
+int cwb(char arr[][MAX_SIZE], Coordinates *coordiantes);
+int cwl(char arr[][MAX_SIZE], Coordinates *coordinates);
+int cwr(char arr[][MAX_SIZE], Coordinates *coordinates);
+void push(Coords coord, Coordinates *coordinates);
+Coords pop(Coordinates *coordinates);
+Coords peek(Coordinates *coordinates);
+void clear(Coordinates *coordinates);
+bjpi
+cjpi
+void backtrack
+rpnt
 
 #endif
